6016ee01ec33ae1c81d64dda67d232052c6985c2
Nguyen <nguyentoankhoa12@gmail.com>
2026-04-17 22:49:53 +0700

    add docker for deployment

--- src/main/java/asset/project/config/CorsConfig.java
+++ src/main/java/asset/project/config/CorsConfig.java
@@ -21,7 +21,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins:http://localhost:3000,http://localhost:5173,http://localhost:8080}")
+    @Value("${FRONTEND_URL}")
     private List<String> allowedOrigins;
 
     @Bean
